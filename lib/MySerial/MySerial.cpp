932d7390ad5a9cc599693e6fc73ee122954606e2
周贤达 <1305287621@qq.com>
2024-12-23 19:59:01 +0800

    完成与stm32的通信

--- lib/MySerial/MySerial.cpp
+++ lib/MySerial/MySerial.cpp
@@ -9,7 +9,9 @@ extern MyData_t Mydata;
 // 发送结构体的函数
 void sendStruct(const SerialSendData_t &data)
 {
-    MySerial_stm32.write((uint8_t *)&data, sizeof(data));
+    //MySerial_stm32.write((uint8_t *)&data, sizeof(data));
+    MySerial_stm32.write((uint8_t *)&data,sizeof(SerialSendData_t));
+    // MySerial_stm32.print("hello");
 }
 // 接收结构体的函数
 bool receiveStruct()
@@ -26,7 +28,7 @@ bool receiveStruct()
         if (index >= sizeof(data))
         {
             index = 0;   // 重置索引，准备接收下一个结构体
-            //Serial.printf("stm32输入: %f  %f  %f  %f  %f  %f %f\n", Mydata.SeriaReceivelData.joint1_aspeed,Mydata.SeriaReceivelData.joint2_aspeed,Mydata.SeriaReceivelData.joint3_aspeed,Mydata.SeriaReceivelData.joint4_aspeed,Mydata.SeriaReceivelData.joint5_aspeed,Mydata.SeriaReceivelData.joint6_aspeed,Mydata.SeriaReceivelData.joint7_aspeed);
+            Serial.printf("stm32输入: %f  %f  %f  %f  %f  %f %f\n", Mydata.SeriaReceivelData.joint1_aspeed,Mydata.SeriaReceivelData.joint2_aspeed,Mydata.SeriaReceivelData.joint3_aspeed,Mydata.SeriaReceivelData.joint4_aspeed,Mydata.SeriaReceivelData.joint5_aspeed,Mydata.SeriaReceivelData.joint6_aspeed,Mydata.SeriaReceivelData.joint7_aspeed);
             return true; // 接收完成
         }
     }
@@ -42,7 +44,7 @@ void MySerial_Init()
 
 void MySerial_Loop()
 {
-    Serial.printf("输出:Button 1 is : %d Button 2 is : %d Button 3 is : %d Button 4 is : %d\n", Mydata.SeriaSendlData.button1,Mydata.SeriaSendlData.button2,Mydata.SeriaSendlData.button3,Mydata.SeriaSendlData.button4);
+    //Serial.printf("输出:Button 1 is : %d Button 2 is : %d Button 3 is : %d Button 4 is : %d\n", Mydata.SeriaSendlData.button1,Mydata.SeriaSendlData.button2,Mydata.SeriaSendlData.button3,Mydata.SeriaSendlData.button4);
     //Serial.println("\n");
     sendStruct(Mydata.SeriaSendlData);
     
--- src/main.cpp
+++ src/main.cpp
@@ -21,3 +21,5 @@ void loop()
 {
   delay(500); 
 }
+
+
